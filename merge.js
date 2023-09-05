12be24117f233d449173bed7e70291c56eb056d2
alexm <alex-m@outlook.com>
2023-09-05 14:45:05 -0700

    Add docstrings and parameters to merge and mergeSort functions

--- merge.js
+++ merge.js
@@ -1,5 +1,15 @@
-function merge() {}
 
-function mergeSort() {}
+
+/** Merge 2 sorted arrays into a single sorted array. */
+function merge(arr1, arr2) {
+
+}
+
+
+/** Sort the given array of numbers using the merge sort algorithm. */
+function mergeSort(array) {
+
+}
+
 
 module.exports = { merge, mergeSort};
