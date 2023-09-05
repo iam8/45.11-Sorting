befba23dcca2d974ac08136a7e016c96d71a9f6b
alexm <alex-m@outlook.com>
2023-09-05 14:44:38 -0700

    Add docstring and beginning of logic to bubbleSort()

--- bubble.js
+++ bubble.js
@@ -1,3 +1,23 @@
-function bubbleSort() {}
+
+
+/** Sort a given array of numbers using the bubble sort algorithm. */
+function bubbleSort(array) {
+
+    for (let i = 0; i < array.length - 1; i++) {
+        const curr = array[i];
+        const next = array[i + 1];
+
+        // Switch current value and the next
+        if (curr > next) {
+            array[i + 1] = curr;
+            array[i] = next;
+        }
+
+
+    }
+
+    return array;
+}
+
 
 module.exports = bubbleSort;
